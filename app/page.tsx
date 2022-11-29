3f8f8792f854bac845e7898df15329959ae385ac
Jordan Pittman <jordan@cryptica.me>
2022-11-29 11:54:39 -0500

    Update readme to demonstrate issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div
+      className="
+        p-2 m-4 rounded-lg
+        bg-green-500
+      "
+    >
+      <div className="bg-white/75 text-gray-700 px-4 py-2 rounded font-medium">
+        Change classes here and then look in the browser
+      </div>
+    </div>
+  );
 }
